f933a4d9dfac600ea2b030c4779f580f21ad8eab
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-22 15:40:40 +0500

    feat: implemented create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import {writeFile} from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
