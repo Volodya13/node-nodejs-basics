6c9b9a34423ff3c7ba38998df5dcdf10999cbb6c
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-23 19:43:17 +0500

    feat: implemented calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+  
+  const content = await readFile(filePath);
+  const hash = createHash('sha256').update(content).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
