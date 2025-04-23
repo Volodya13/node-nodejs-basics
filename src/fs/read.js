ea769883db7a4ef51ae557518cf87cffa267dd4d
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-23 17:09:42 +0500

    feat: implemented read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import {readFile} from 'fs/promises';
+import { log } from 'console';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  try {
+    console.log(await readFile(filePath, 'utf8'));
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
