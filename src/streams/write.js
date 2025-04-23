deac5050ade819066f1778618ee2b621ea03c72a
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-23 20:08:24 +0500

    feat: implemented streams write.js function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fileToWrite.txt`;
+
+  try {
+    await pipeline(
+      process.stdin,
+      createWriteStream(filePath, {flags: 'a', encoding: 'utf-8'}),
+    );
+  } catch (err) {
+    throw new Error(`Error writing file: ${err.message}`);
+  }
 };
 
 await write();
