c1c93549b0411231665e209a6dca05f543bc3423
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-23 20:07:54 +0500

    feat: implemented streams read.js function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fileToRead.txt`;
+
+  try {
+    await pipeline(
+      createReadStream(filePath, { encoding: 'utf-8' }),
+      process.stdout,
+    );
+  } catch (err) {
+    throw new Error(`Error reading file: ${err.message}`);
+  }
 };
 
 await read();
