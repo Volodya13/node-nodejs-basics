a43d63c24790a977859397fafcf1082668986b79
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-25 14:04:09 +0500

    feat: implemented streams transform.js function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import {pipeline} from 'stream/promises';
+import {Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStrem = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString()
+        .split('').reverse().join(''));
+    }
+  });
+
+  await pipeline(
+    process.stdin,
+    transformStrem,
+    process.stdout,
+  );
 };
 
 await transform();
