a60d6fa1f50bba6bdfeffa972f6a42bf72e90ee1
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-28 19:35:48 +0500

    feat: implemented zip decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const src = join(__dirname, 'files', 'archive.gz');
+  const dest = join(__dirname, 'files', 'fileToCompress.txt');
+
+  const readStream = createReadStream(src);
+  const gzip = createGzip();
+  const writeStream = createWriteStream(dest);
+
+  try {
+    await pipeline(
+      readStream,
+      gzip,
+      writeStream,
+    );
+    console.log('File decompressed successfully!');
+  } catch (err) {
+    console.error('Decompression failed:', err);
+    throw new Error('FS operation failed');
+  }
+
 };
 
 await decompress();
