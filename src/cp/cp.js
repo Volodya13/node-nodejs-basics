637f5bae35b6731fc53046437284235a0b9a4816
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-04-28 19:57:29 +0500

    feat: implemented spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'script.js');
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(filePath, args, { stdio: ['pipe', 'pipe', 'inherit', 'ipc'] });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( /*['someArgument1', 'someArgument2', 'someArgument3'] */);
