1f9b7f6c120977414ebbb33ff457110b2304a79e
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-10-26 18:00:50 +0500

    feat: implement `calculateHash` for nodejs basics task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import {createHash} from 'crypto'
+import {createReadStream} from 'fs'
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('error', (err) => {
+    console.log(err);
+    throw new Error('FS operation failed');
+  });
+  stream.on('data', (chunk) => hash.update(chunk));
+  stream.on('end', () => {
+    console.log(hash.digest('hex'));
+  })
 };
 
 await calculateHash();
