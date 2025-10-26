748e987bd8f1ab296b84567f42622288b6c1d5b9
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-10-26 19:12:56 +0500

    feat: implement zip for nodejs basics task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const inputFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+const outputFilePath = join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-  // Write your code here
+  await pipeline(
+    createReadStream(inputFilePath),
+    createGzip(),
+    createWriteStream(outputFilePath),
+  )
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const restoreFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+const archiveFilePath = join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-  // Write your code here
+  await pipeline(
+    createReadStream(archiveFilePath),
+    createGunzip(),
+    createWriteStream(restoreFilePath),
+  );
 };
 
-await decompress();
+await decompress().catch(err => {
+    console.error('Decompression failed:', err);
+});
