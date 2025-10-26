8fed1afde22dcd9ef4def4f68e050ef15cb84d80
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-10-26 19:47:49 +0500

    feat: implement cp(child process) for nodejs basics task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import {fork} from 'child_process';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+	const childProcess = fork(filePath, args, { stdio: ['pipe', 'pipe', 'inherit', 'ipc'] });
+	
+	process.stdin.pipe(childProcess.stdin);
+	childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( /*['someArgument1', 'someArgument2', 'someArgument3']*/);
