3a2bd4bcb44312d329508c1e478336fae66f77e5
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-10-26 17:10:41 +0500

    feat: implement cli functions for nodejs basics task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-  // Write your code here
+  const agrs = process.argv.slice(2);
+  const options = [];
+
+  for (let i = 0; i < agrs.length; i += 2) {
+    const key = agrs[i].replace(/--/, '');
+    const value = agrs[i + 1];
+
+    options.push(`${key} is ${value}`);
+  }
+
+    console.log(options.join(', '));
 };
 
 parseArgs();
